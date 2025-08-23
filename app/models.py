8442abbfdf7e8a2f02c0418fd1ad844a047d52c9
seongjoow <seongjoow@gmail.com>
2025-08-23 19:03:34 +0900

    Feat(report) : 리포트 생성 요청 DTO에 리뷰 데이터 추가

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime, date
+
+class OrderItemRow(BaseModel):
+    orderItemId: str
+    menuName: str
+    price: int
+    quantity: int
+
+class OrderRow(BaseModel):
+    orderId: str
+    totalPrice: int
+    orderChannel: str
+    receiptMethod: str
+    paymentMethod: str
+    orderStatus: str
+    requestMessage: Optional[str] = None
+    createdAt: datetime
+    storeName: str
+    usersex: Optional[str] = None
+    birthdate: Optional[date] = None
+    isFirstOrderInStore: bool
+    orderSeqInStore: int
+    items: List[OrderItemRow]
+
+class ReviewRow(BaseModel):
+    rating: int
+    content: str
+    createdAt: datetime
+
+class ReportGenerationRequest(BaseModel):
+    storeId: str
+    orders: List[OrderRow]
+    reviews: List[ReviewRow]
+
+
+class OrderPayload(BaseModel):
+    storeId: str
+    orders: List[OrderRow]
+    
+class ReviewPayload(BaseModel):
+    storeId: str
+    reviews: List[ReviewRow]
+
+class GenerateJsonResponse(BaseModel):
+    url: str
+    localPath: str
+    createdAt: str
